b3f8033965beb073c00985e33ffbf49a355a186c
karamalqinneh <karamalqinneh@live.com>
2022-05-20 22:14:20 +0300

    updated response

--- src/routes/auth-routes/signin.route.js
+++ src/routes/auth-routes/signin.route.js
@@ -4,7 +4,11 @@ const basicAuth = require("../../middleware/auth/basic.auth");
 
 // signup Function
 function signinController(req, res) {
-  res.status(200).json({ username: req.user.username, token: req.user.token });
+  res.status(200).json({
+    username: req.user.username,
+    token: req.user.token,
+    action: req.user.actions,
+  });
 }
 
 module.exports = router.post("/signin", basicAuth, signinController);
